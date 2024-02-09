1e96b0c603b7447a2f988b98c84bce65c9b3a4fa
lass6230 <due6230@gmail.com>
2024-02-09 11:37:04 +0100

    i

--- build/ros2_controllers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lass6230/github/ros2_controllers_mecanum/ros2_controllers
-# Build directory: /home/lass6230/github/ros2_controllers_mecanum/build/ros2_controllers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ros2_controllers/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/lass6230/github/ros2_controllers_mecanum/install/mecanum_drive_controller/share/mecanum_drive_controller/package.sh"
